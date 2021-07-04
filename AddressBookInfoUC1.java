92a6886a5c50eb45c4cea3f773d68619bf38523a
dhirajp504@gmail.com <dhirajp504@gmail.com>
2021-07-04 15:03:14 +0530

    Added: address book info is added

---
+++ AddressBookInfoUC1.java
@@ -0,0 +1,25 @@
+package address_book;
+
+public class AddressBookInfoUC1 {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO ADDRESS BOOK PROGRAMS");
+	}
+}
+
+class CreatContacts {
+	String fName, lName, address, city, state;
+	Long zipCode, phoneNum;
+	String email;
+
+	CreatContacts(String fName, String lName, String address, String city, String state, long zipCode, long phoneNum,
+			String email) {
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNum = phoneNum;
+		this.email = email;
+	}
+}
